22d1753278003b543e829ee73982fc1855969e24
surith <surith1234@gmail.com>
2023-05-14 20:04:24 +0530

    edit

--- anusha73k/forecastApp.py
+++ anusha73k/forecastApp.py
@@ -15,19 +15,23 @@ def app():
 
     # If a file was uploaded, read it into a Pandas dataframe
     if uploaded_file is not None:
-        y_test = pd.read_csv(uploaded_file, index_col=0, parse_dates=True)
+        data = pd.read_csv(uploaded_file, index_col=0, parse_dates=True)
 
+        train = data.loc[data.index < '2021-03-31']
+        test = data.loc[data.index >= '2021-03-31']
+        
         # Load the Exponential Smoothing model from a pickle file
         with open('ES_model.pkl', 'rb') as f:
             model_ES = pickle.load(f)
 
         # Generate forecasts for the testing set
-        y_pred_ses = model_ES.forecast(len(y_test))
+        y_pred_ses = model_ES.forecast(len(test))
 
         # Plot the actual values and forecast
         plt.figure(figsize=(12,6))
-        plt.plot(y_test.index, y_test.values, label='Actual')
-        plt.plot(y_test.index, y_pred_ses, label='Forecast')
+        plt.plot(train.index, train.values, label='Actual Train')
+        plt.plot(test.index, test.values, label='Actual Test')
+        plt.plot(test.index, y_pred_ses, label='Forecast')
         plt.legend()
         plt.title('Exponential Smoothing Forecast')
         plt.xlabel('Date')
